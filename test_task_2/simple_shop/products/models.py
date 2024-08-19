52ae92255ad12291c6e78a02c4e740509c42b712
Eduard Gumen <hydrospirt@mail.ru>
2024-08-19 20:53:31 +0300

    Add Category, SubCategory, Product

--- test_task_2/simple_shop/products/models.py
+++ test_task_2/simple_shop/products/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from PIL import Image
+import os
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    models.SlugField(max_length=15, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=15, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL,
+                                 null=True, blank=True)
+    subcategory = models.ForeignKey(SubCategory,
+                                    on_delete=models.SET_NULL,
+                                    null=True, blank=True)
+    image = models.ImageField(upload_to='products/',
+                              null=True, blank=True)
+    image_small = models.ImageField(upload_to='products/',
+                                    null=True, blank=True)
+    image_medium = models.ImageField(upload_to='products/',
+                                     null=True, blank=True)
+    image_large = models.ImageField(upload_to='products/',
+                                    null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.image:
+            self.create_thumbnails()
+
+    def create_thumbnails(self, *args, **kwargs):
+        img = Image.open(self.image.path)
+
+        sizes = {
+            "small": (100, 100),
+            "medium": (300, 300),
+            "large": (600, 600),
+        }
+
+        for size_name, size in sizes.items():
+            img_copy = img.copy()
+            img_copy.thumbnail(size)
+            thumb_nail_path = os.path.join(
+                os.path.dirname(self.image.path),
+                f"{os.path.basename(self.image.name).split(".")[0]}_"
+                + f"{size_name}.{self.image.name.split(".")[-1]}"
+            )
+            img_copy.save(thumb_nail_path)
+
+            setattr(self, f"image_{size_name}", thumb_nail_path)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
