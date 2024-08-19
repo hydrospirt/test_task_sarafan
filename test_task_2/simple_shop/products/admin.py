3aa7f7112b2c4d3af1bdf3d4c2c186137b96e9cf
Eduard Gumen <hydrospirt@mail.ru>
2024-08-19 21:05:27 +0300

    Add CategoryAdmin, SubCategoryAdmin, ProductAdmin

--- test_task_2/simple_shop/products/admin.py
+++ test_task_2/simple_shop/products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, SubCategory, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name")
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "subcategory")
+    search_fields = ("name")
+    list_filter = ("id", "price")
