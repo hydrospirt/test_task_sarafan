e9792dde0740fd27be92988f76beac83eea859c2
Eduard Gumen <hydrospirt@mail.ru>
2024-08-20 19:34:25 +0300

    Fix tuple name

--- test_task_2/simple_shop/products/admin.py
+++ test_task_2/simple_shop/products/admin.py
@@ -6,17 +6,17 @@ from .models import Category, SubCategory, Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
-    search_fields = ("name")
+    search_fields = ("name", )
 
 
 @admin.register(SubCategory)
 class SubCategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
-    search_fields = ("name")
+    search_fields = ("name", )
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "price", "subcategory")
-    search_fields = ("name")
+    search_fields = ("name", )
     list_filter = ("id", "price")
