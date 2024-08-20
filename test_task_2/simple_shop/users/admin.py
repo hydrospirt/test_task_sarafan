051f97adb1f1ee02552a4f95c212350f6f039cba
Eduard Gumen <hydrospirt@mail.ru>
2024-08-20 19:21:26 +0300

    Add CustomUser

--- test_task_2/simple_shop/users/admin.py
+++ test_task_2/simple_shop/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("id",
+                    "email"
+                    "phone_number",
+                    "role",
+                    "is_active",
+                    "is_staff")
--- test_task_2/simple_shop/users/models.py
+++ test_task_2/simple_shop/users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    phone = PhoneNumberField(
+        unique=True,
+        null=False,
+        blank=False)
+
+    def __str__(self):
+        return self.username
