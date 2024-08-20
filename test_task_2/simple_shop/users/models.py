9fc7f008f5f3f66c6716d7b10ee0b9f4fbc327ae
Eduard Gumen <hydrospirt@mail.ru>
2024-08-20 20:12:27 +0300

    Add groups and user_permissions, fix fields

--- test_task_2/simple_shop/users/models.py
+++ test_task_2/simple_shop/users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Permission, Group
 from django.db import models
 
 from phonenumber_field.modelfields import PhoneNumberField
@@ -7,10 +7,31 @@ from phonenumber_field.modelfields import PhoneNumberField
 class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
-    phone = PhoneNumberField(
+    phone_number = PhoneNumberField(
         unique=True,
         null=False,
         blank=False)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ("username", )
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name="groups",
+        blank=True,
+        help_text="Группы, к которым принадлежит этот пользователь.",
+        related_name="customuser_set",
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name="user permissions",
+        blank=True,
+        help_text="Определенные разрешения для этого пользователя.",
+        related_name="customuser_set",
+        related_query_name="user",
+    )
 
     def __str__(self):
         return self.username
