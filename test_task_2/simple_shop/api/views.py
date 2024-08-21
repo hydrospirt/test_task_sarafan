3804e076cf57af2c9c72125fc90cdc8a7193d8bb
Eduard Gumen <hydrospirt@mail.ru>
2024-08-21 20:27:52 +0300

    Change ModelVieSet to ReadOnlyModelViewSet, add get_serializer_context

--- test_task_2/simple_shop/api/views.py
+++ test_task_2/simple_shop/api/views.py
@@ -1,17 +1,22 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from products.models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 from .pagination import StandardResultsSetPagination
 
 
-class CategoryViewSet(ModelViewSet):
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     pagination_class = StandardResultsSetPagination
 
 
-class ProductViewSet(ModelViewSet):
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = StandardResultsSetPagination
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
