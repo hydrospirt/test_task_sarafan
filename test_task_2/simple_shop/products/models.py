4660e409d1def7b7456b9a57883b92f205d3381e
Eduard Gumen <hydrospirt@mail.ru>
2024-08-21 19:33:52 +0300

    Delete useless imports

--- test_task_2/simple_shop/products/models.py
+++ test_task_2/simple_shop/products/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
 from django.utils.text import slugify
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 from PIL import Image
 import os
 
