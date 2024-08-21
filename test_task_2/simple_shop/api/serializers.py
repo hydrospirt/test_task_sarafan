a41cbcdfc7f9ed3532be0e819a59d93921a2dfd1
Eduard Gumen <hydrospirt@mail.ru>
2024-08-21 18:48:24 +0300

    Add SubCategorySerializer, CategorySerializer, ProductSerializer

---
+++ test_task_2/simple_shop/api/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from products.models import Category, SubCategory, Product
+
+
+class SubCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubCategory
+        fields = ("id", "name", "slug", "image")
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    subcategories = SubCategorySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ("id", "name", "slug", "image", "subcategories")
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field="slug",
+                                            read_only=True)
+    subcategory = serializers.SlugRelatedField(slug_field="slug",
+                                               read_only=True)
+    images = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = ("id", "name", "slug", "category",
+                  "subcategory", "price", "images")
+
+    def get_images(self, obj):
+        return {
+            "small": obj.image_small.url if obj.image_small else None,
+            "medium": obj.image_medium.url if obj.image_medium else None,
+            "large": obj.image_large.url if obj.image_large else None,
+        }
