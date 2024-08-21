462531390c7ec681d5d0cd23b31a415383844fa0
Eduard Gumen <hydrospirt@mail.ru>
2024-08-21 18:48:49 +0300

    Add CategoryViewSet, ProductViewSet

--- test_task_2/simple_shop/api/views.py
+++ test_task_2/simple_shop/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from products.models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+from .pagination import StandardResultsSetPagination
 
-# Create your views here.
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = StandardResultsSetPagination
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = StandardResultsSetPagination
