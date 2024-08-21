e69532120a622e6f3ee6b667aa5ab89c4c6e0120
Eduard Gumen <hydrospirt@mail.ru>
2024-08-21 18:49:34 +0300

    Add DefaultRouter and paths for categories and products

---
+++ test_task_2/simple_shop/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from .views import CategoryViewSet, ProductViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet)
+
+urlpatterns = router.urls
