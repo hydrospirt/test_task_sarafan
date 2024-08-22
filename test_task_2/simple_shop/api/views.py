0c669c5961633862b6990b8f45059fdf436bc512
Eduard Gumen <hydrospirt@mail.ru>
2024-08-22 21:35:53 +0300

    Add CartAPIView(APIView)

--- test_task_2/simple_shop/api/views.py
+++ test_task_2/simple_shop/api/views.py
@@ -1,8 +1,15 @@
-from rest_framework import viewsets
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from products.models import Category, Product
-from .serializers import CategorySerializer, ProductSerializer
+
 from .pagination import StandardResultsSetPagination
+from .serializers import (CartItemSerializer, CartSerializer,
+                          CategorySerializer, ProductSerializer)
+from .service import Cart
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
@@ -18,5 +25,74 @@ class ProductViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
-        context['request'] = self.request
+        context["request"] = self.request
         return context
+
+
+class CartAPIView(APIView):
+    @swagger_auto_schema(
+        operation_description="Получить текущую корзину",
+        responses={200: CartSerializer()}
+    )
+    def get(self, request):
+        cart = Cart(request)
+
+        return Response(
+            {"data": list(cart.__iter__()),
+             "cart_total_price": cart.get_total_price()},
+            status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_description="Добавить продукт в корзину",
+        request_body=CartItemSerializer,
+        responses={201: CartItemSerializer()}
+    )
+    def post(self, request):
+        serializer = CartItemSerializer(data=request.data)
+        if serializer.is_valid():
+            product = serializer.validated_data["product_id"]
+            quantity = serializer.validated_data["quantity"]
+            cart = Cart(request)
+            cart.add(product, quantity)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(
+        operation_description="Изменить количество продуктов в коризине",
+        request_body=CartItemSerializer,
+        responses={200: CartItemSerializer()}
+    )
+    def put(self, request):
+        serializer = CartItemSerializer(data=request.data)
+        if serializer.is_valid():
+            product = serializer.validated_data['product_id']
+            quantity = serializer.validated_data['quantity']
+            cart = Cart(request)
+            cart.add(product, quantity, override_quantity=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(
+        operation_description="Удалить продукт из корзины",
+        request_body=CartItemSerializer,
+        responses={204: "No Content"}
+    )
+    def delete(self, request):
+        serializer = CartItemSerializer(data=request.data)
+        if serializer.is_valid():
+            product = serializer.validated_data["product_id"]
+            cart = Cart(request)
+            cart.remove(product)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ClearCartAPIView(APIView):
+    @swagger_auto_schema(
+        operation_description="Очистить корзину",
+        responses={204: "No Content"}
+    )
+    def delete(self, request):
+        cart = Cart(request)
+        cart.clear()
+        return Response(status=status.HTTP_204_NO_CONTENT)
