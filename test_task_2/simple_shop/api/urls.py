305fd1041a2e30df56551afbfd3f8d5ca8fa807e
Eduard Gumen <hydrospirt@mail.ru>
2024-08-22 21:32:15 +0300

    Add new paths for cart

--- test_task_2/simple_shop/api/urls.py
+++ test_task_2/simple_shop/api/urls.py
@@ -2,12 +2,14 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, ProductViewSet
+from .views import CategoryViewSet, ProductViewSet, CartAPIView, ClearCartAPIView
 
 router = DefaultRouter()
-router.register(r'categories', CategoryViewSet)
-router.register(r'products', ProductViewSet)
+router.register(r"categories", CategoryViewSet)
+router.register(r"products", ProductViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
+    path("cart/", CartAPIView.as_view(), name="cart"),
+    path("cart/clear/", ClearCartAPIView.as_view(), name="cart-clear"),
 ]
