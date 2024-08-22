18e98e425b3eadc8780cf38c805a6e79da4feaab
Eduard Gumen <hydrospirt@mail.ru>
2024-08-22 21:30:47 +0300

    Add SESSION_ENGINE, SESSION_COOKIE_NAME, CART_SESSION_ID

--- test_task_2/simple_shop/simple_shop/settings.py
+++ test_task_2/simple_shop/simple_shop/settings.py
@@ -131,3 +131,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
+SESSION_COOKIE_NAME = "cart_session"
+CART_SESSION_ID = "cart"
