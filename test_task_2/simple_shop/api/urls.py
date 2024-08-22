135138e0f3926b75a70ca0d25248fe8e05e8bc80
Eduard Gumen <hydrospirt@mail.ru>
2024-08-22 22:13:56 +0300

    Add paths for tokens

--- test_task_2/simple_shop/api/urls.py
+++ test_task_2/simple_shop/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .views import (CartAPIView, CategoryViewSet, ClearCartAPIView,
                     ProductViewSet)
@@ -11,6 +15,8 @@ router.register(r"products", ProductViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("cart/", CartAPIView.as_view(), name="cart"),
     path("cart/clear/", ClearCartAPIView.as_view(), name="cart-clear"),
 ]
