d58600b0de89af35f74b9e0fdd7598001ecaab55
Eduard Gumen <hydrospirt@mail.ru>
2024-08-23 20:49:54 +0300

    Fix STATIC_URL, MEDIA_URL

--- test_task_2/simple_shop/simple_shop/settings.py
+++ test_task_2/simple_shop/simple_shop/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 AUTH_USER_MODEL = "users.CustomUser"
