da2218505b2f98d47ca26fffb6ca1a090c8efb6d
Eduard Gumen <hydrospirt@mail.ru>
2024-08-24 12:48:12 +0300

    Add JWTAuthentication for Session, update docstrings, add logger

--- test_task_2/simple_shop/api/service.py
+++ test_task_2/simple_shop/api/service.py
@@ -1,27 +1,40 @@
+import logging
 from decimal import Decimal
+
 from django.conf import settings
-from .serializers import ProductSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from products.models import Product
+
+from .serializers import ProductSerializer
+
+logger = logging.getLogger(__name__)
 
 
 class Cart:
     def __init__(self, request):
         """
-        Initialize the cart
+        Инициализирует корзину.
         """
         self.session = request.session
-        cart = self.session.get(settings.CART_SESSION_ID)
+        jwt_auth = JWTAuthentication()
+        user, _ = jwt_auth.authenticate(request)
+        self.user_id = str(user.id) if user else None
+        cart = self.session.get(settings.CART_SESSION_ID,
+                                {}).get(self.user_id, {})
         if not cart:
-            # Save an empty cart in session
-            cart = self.session[settings.CART_SESSION_ID] = {}
-        self.cart = cart
+            self.session.setdefault(settings.CART_SESSION_ID,
+                                    {})[self.user_id] = {}
+        self.cart = self.session[settings.CART_SESSION_ID][self.user_id]
+
+        logger.info(f"Session ID: {self.user_id}, Cart: {self.cart}")
 
     def save(self):
         self.session.modified = True
 
     def add(self, product, quantity=1, override_quantity=False):
         """
-        Add product to the cart or update its quantity
+        Добавить товар в корзину или обновить его количество
         """
         product_id = str(product.id)
         if product_id not in self.cart:
@@ -37,7 +50,7 @@ class Cart:
 
     def remove(self, product):
         """
-        Remove a product from the cart
+        Удалить товар из корзины
         """
         product_id = str(product.id)
         if product_id in self.cart:
@@ -46,9 +59,10 @@ class Cart:
 
     def __iter__(self):
         """
-        Loop through cart items and fetch the products from the database
+        Просмотреть товары в корзине и извлечь их из базы данных.
         """
-        product_ids = self.cart.keys()
+        product_ids = [key for key in self.cart.keys()
+                       if key not in ["quantity", "price"]]
         products = Product.objects.filter(id__in=product_ids)
         cart = self.cart.copy()
         for product in products:
@@ -60,7 +74,7 @@ class Cart:
 
     def __len__(self):
         """
-        Count all items in the cart
+        Подсчитать все товары в корзине.
         """
         return sum(item["quantity"] for item in self.cart.values())
 
@@ -69,6 +83,8 @@ class Cart:
                    * item["quantity"] for item in self.cart.values())
 
     def clear(self):
-        # Remove cart from session
+        """
+        Очистка корзины из сессии.
+        """
         del self.session[settings.CART_SESSION_ID]
         self.save()
